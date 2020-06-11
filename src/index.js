150c009a64c1f17ac8c365c2abd8b57e1be9e82a
gn5r <shangyuan.tuolang@gmail.com>
2020-06-11 13:49:42 +0900

    release beta.3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import Confirm from "@/components/Confirm";
-import mixin from "../lib/mixin";
 
 export function install(Vue) {
   if (install.installed) return;
   install.installed = true;
   Vue.component("confirm", Confirm);
-  Vue.mixin(mixin);
 }
 
 export default Confirm;
