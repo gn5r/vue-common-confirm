0f1d5884055ff6398ea25050e46926dd14482ec7
gn5r <shangyuan.tuolang@gmail.com>
2020-06-12 14:27:41 +0900

    Revert "release beta.3"
    This reverts commit 150c009a64c1f17ac8c365c2abd8b57e1be9e82a.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Confirm from "@/components/Confirm";
+import mixin from "../lib/mixin";
 
 export function install(Vue) {
   if (install.installed) return;
   install.installed = true;
   Vue.component("confirm", Confirm);
+  Vue.mixin(mixin);
 }
 
 export default Confirm;
