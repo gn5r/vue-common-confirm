42bb76af53aaf1921f90cd4637044ebb4b528d67
gn5r <shangyuan.tuolang@gmail.com>
2020-06-15 11:49:16 +0900

    module化メソッドを修正

--- src/index.js
+++ src/index.js
@@ -2,11 +2,25 @@ import Confirm from "@/components/Confirm";
 import mixin from "../lib/mixin";
 
 // Vue.use時に実行される関数を定義
-Confirm.install = function(Vue) {
+export function install(Vue) {
   if (install.installed) return;
   install.installed = true;
   Vue.component("confirm", Confirm);
   Vue.mixin(mixin);
+}
+
+const plugin = {
+  install,
 };
+
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
 
 export default Confirm;
