f8bc5109d744a13a79c2bd7be477118b1758f05e
gn5r <shangyuan.tuolang@gmail.com>
2022-02-12 21:48:22 +0900

    style(vetur-helper-json): lint

--- packages/vetur-helper-json/src/index.js
+++ packages/vetur-helper-json/src/index.js
@@ -1,120 +1,113 @@
-const fs = require('fs')
-const Vue = require('vue')
-const VConfirm = require('@gn5r/vue-common-confirm')
-const shell = require('shelljs')
+const fs = require("fs");
+const Vue = require("vue");
+const VConfirm = require("@gn5r/vue-common-confirm");
+const shell = require("shelljs");
 
-if (VConfirm === undefined) throw new Error('VConfirm is undefined')
+if (VConfirm === undefined) throw new Error("VConfirm is undefined");
 
-Vue.component('v-confirm', VConfirm)
+Vue.component("v-confirm", VConfirm);
 
-const hyphenateRE = /\B([A-Z])/g
+const hyphenateRE = /\B([A-Z])/g;
 
-function hyphenate (str) {
-  return str.replace(hyphenateRE, '-$1').toLowerCase()
+function hyphenate(str) {
+  return str.replace(hyphenateRE, "-$1").toLowerCase();
 }
 
-function parseFunctionParams (func) {
-  const groups = /function\s_.*\((.*)\)\s\{.*/i.exec(func)
-  if (groups && groups.length > 1) return `(${groups[1]}) => {}`
-  else return 'null'
+function parseFunctionParams(func) {
+  const groups = /function\s_.*\((.*)\)\s\{.*/i.exec(func);
+  if (groups && groups.length > 1) return `(${groups[1]}) => {}`;
+  else return "null";
 }
 
-function getPropType (type) {
+function getPropType(type) {
   if (Array.isArray(type)) {
-    return type.map((t) => getPropType(t))
+    return type.map((t) => getPropType(t));
   }
 
-  if (!type) return 'any'
+  if (!type) return "any";
 
-  return type.name.toLowerCase()
+  return type.name.toLowerCase();
 }
 
-function getPropDefault (def, type) {
+function getPropDefault(def, type) {
   if (
-    def === '' ||
-    (def == null && type !== 'boolean' && type !== 'function')
+    def === "" ||
+    (def == null && type !== "boolean" && type !== "function")
   ) {
-    return 'undefined'
-  } else if (typeof def === 'function' && type !== 'function') {
-    def = def.call({})
+    return "undefined";
+  } else if (typeof def === "function" && type !== "function") {
+    def = def.call({});
   }
 
-  if (type === 'boolean') {
-    return def ? 'true' : 'false'
+  if (type === "boolean") {
+    return def ? "true" : "false";
   }
 
-  if (type === 'string') {
-    return def ? `'${def}'` : def
+  if (type === "string") {
+    return def ? `'${def}'` : def;
   }
 
-  if (type === 'function') {
-    return parseFunctionParams(def)
+  if (type === "function") {
+    return parseFunctionParams(def);
   }
 
-  return def
+  return def;
 }
 
-function genProp (name, prop) {
-  const type = getPropType(prop.type)
+function genProp(name, prop) {
+  const type = getPropType(prop.type);
 
   return {
     name,
     type,
-    description: prop.description || '',
-    default: getPropDefault(prop.default, type)
-  }
+    description: prop.description || "",
+    default: getPropDefault(prop.default, type),
+  };
 }
 
-function parseComponent (component) {
+function parseComponent(component) {
   return {
     props: parseProps(component),
-    description: parseDescription(component)
-  }
+    description: "",
+  };
 }
 
-function parseProps (component, array = []) {
-  const options = component.options
-  const props = options.props || {}
+function parseProps(component, array = []) {
+  const options = component.options;
+  const props = options.props || {};
 
   Object.keys(props).forEach((key) => {
-    const generated = genProp(key, props[key], component.name)
-    array.push(generated)
-  })
+    const generated = genProp(key, props[key], component.name);
+    array.push(generated);
+  });
 
-  return array.sort((a, b) => a.name > b.name)
+  return array.sort((a, b) => a.name > b.name);
 }
 
-function parseDescription (component) {
-  const options = component.options
-  const description = options.description || {}
+function writeJsonFile(obj, file) {
+  const stream = fs.createWriteStream(file);
 
-  return description
+  stream.once("open", () => {
+    stream.write(JSON.stringify(obj, null, 2));
+    stream.end();
+  });
 }
 
-function writeJsonFile (obj, file) {
-  const stream = fs.createWriteStream(file)
+const components = {};
 
-  stream.once('open', () => {
-    stream.write(JSON.stringify(obj, null, 2))
-    stream.end()
-  })
-}
+const installedComponents = Vue.options.components;
 
-const components = {}
-
-const installedComponents = Vue.options.components
-
-const componentNameRegex = /^v-confirm/
+const componentNameRegex = /^v-confirm/;
 
 for (const name in installedComponents) {
-  if (!componentNameRegex.test(name)) continue
+  if (!componentNameRegex.test(name)) continue;
 
-  const component = installedComponents[name]
+  const component = installedComponents[name];
 
-  const kebabName = hyphenate(name)
-  const options = parseComponent(component.options.default)
+  const kebabName = hyphenate(name);
+  const options = parseComponent(component.options.default);
 
-  components[kebabName] = options
+  components[kebabName] = options;
 }
 
 const tags = Object.keys(components).reduce((t, k) => {
@@ -122,44 +115,46 @@ const tags = Object.keys(components).reduce((t, k) => {
     attributes: components[k].props
       .map((p) => p.name.replace(/([A-Z])/g, (g) => `-${g[0].toLowerCase()}`))
       .sort(),
-    description: components[k].description
-  }
+    description: components[k].description,
+  };
 
-  return t
-}, {})
+  return t;
+}, {});
 
 const attributes = Object.keys(components).reduce((attrs, k) => {
   const tmp = components[k].props.reduce((a, prop) => {
-    let type = prop.type
+    let type = prop.type;
 
-    if (!type) type = ''
-    else if (Array.isArray(type)) { type = type.map((t) => t.toLowerCase()).join('|') } else type = type.toLowerCase()
+    if (!type) type = "";
+    else if (Array.isArray(type)) {
+      type = type.map((t) => t.toLowerCase()).join("|");
+    } else type = type.toLowerCase();
 
-    const name = prop.name.replace(/([A-Z])/g, (g) => `-${g[0].toLowerCase()}`)
+    const name = prop.name.replace(/([A-Z])/g, (g) => `-${g[0].toLowerCase()}`);
 
     a[`${k}/${name}`] = {
       type,
-      description: prop.description
-    }
+      description: prop.description,
+    };
 
-    return a
-  }, {})
+    return a;
+  }, {});
 
-  return Object.assign(attrs, tmp)
-}, {})
+  return Object.assign(attrs, tmp);
+}, {});
 
-if (!fs.existsSync('dist')) {
-  fs.mkdirSync('dist', 0o755)
+if (!fs.existsSync("dist")) {
+  fs.mkdirSync("dist", 0o755);
 }
 
-writeJsonFile(tags, 'dist/tags.json')
-writeJsonFile(attributes, 'dist/attributes.json')
+writeJsonFile(tags, "dist/tags.json");
+writeJsonFile(attributes, "dist/attributes.json");
 
-console.debug('pwd')
-shell.exec('pwd')
-shell.cp('./dist/tags.json', '../v-confirm/dist')
-shell.cp('./dist/attributes.json', '../v-confirm/dist')
-console.debug('ls')
-shell.exec('ls ../v-confirm/dist')
+console.debug("pwd");
+shell.exec("pwd");
+shell.cp("./dist/tags.json", "../v-confirm/dist");
+shell.cp("./dist/attributes.json", "../v-confirm/dist");
+console.debug("ls");
+shell.exec("ls ../v-confirm/dist");
 
-console.debug('tags.jsonとattributes.jsonを作成しました')
+console.debug("tags.jsonとattributes.jsonを作成しました");
