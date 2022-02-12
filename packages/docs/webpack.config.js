942bf876633752094d9466c8b57cde96c47dbbe9
gn5r <shangyuan.tuolang@gmail.com>
2022-02-12 21:47:28 +0900

    chore(docs): consolidate webpack settings into one

--- packages/docs/webpack.config.js
+++ packages/docs/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
+import TerserPlugin from "terser-webpack-plugin";
 const { VueLoaderPlugin } = require("vue-loader");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 const { VuetifyLoaderPlugin } = require("vuetify-loader");
 
 const resolve = (dir = "") => path.resolve(__dirname, dir);
@@ -11,13 +13,15 @@ const isProcudtion = process.env.NODE_ENV === "production";
  * @type {import("webpack").Configuration}
  */
 module.exports = {
+  mode: isProcudtion ? "production" : "development",
   devtool: isProcudtion ? false : "inline-source-map",
+  target: ["web", "es5"],
   entry: "./src/main.ts",
   output: {
     clean: true,
-    path: resolve("../docs"),
-    filename: "index.js",
-    publicPath: "/vue-common-confirm/",
+    path: resolve("docs"),
+    filename: "js/[name].[chunkhash].js",
+    publicPath: "",
   },
   resolve: {
     alias: {
@@ -52,7 +56,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["vue-style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "vue-style-loader", "css-loader"],
       },
       {
         test: /\.s(c|a)ss$/,
@@ -72,9 +76,49 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    usedExports: false,
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        include: /\.min\.js$/,
+        extractComments: "all",
+        terserOptions: { compress: { drop_console: true } },
+      }),
+    ],
+  },
   plugins: [
-    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      hash: false,
+      template: "./src/index.html",
+      minify: {
+        removeComments: isProcudtion,
+        collapseWhitespace: isProcudtion,
+        removeAttributeQuotes: isProcudtion,
+        minifyJS: isProcudtion,
+        minifyCSS: isProcudtion,
+        minifyURLs: isProcudtion,
+      },
+    }),
     new VueLoaderPlugin(),
     new VuetifyLoaderPlugin(),
   ],
+  devServer: {
+    host: process.env.HOST || "localhost",
+    port: process.env.PORT || 8080,
+    open: process.env.BASE_URL || "/vue-common-confirm",
+    hot: true,
+    static: {
+      directory: path.join(__dirname, "../src"),
+      publicPath: process.env.BASE_URL || "/vue-common-confirm",
+      watch: true,
+    },
+  },
 };
+
+if (isProcudtion) {
+  module.exports.plugins.push(
+    new MiniCssExtractPlugin({ filename: "css/demo-[chunkhash].css" })
+  );
+}
